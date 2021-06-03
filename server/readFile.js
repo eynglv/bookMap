22a90f914b7fdd189ba992cc7cf05549b579c4d1
Elvy Yang <hyang6@wellesley.edu>
2021-06-03 21:09:49 +0800

    functionality: adding to db

--- server/readFile.js
+++ server/readFile.js
@@ -1,15 +1,30 @@
 const fs = require("fs");
+const Book = require("./db/book");
+const db = require("./db/database");
 
 async function read(path) {
   const dir = await fs.promises.opendir(path);
   for await (const dirent of dir) {
     if (dirent.name === "0.json") {
-      const contents = await fs.promises.readFile(`${path}/${dirent.name}`);
-      console.log(JSON.parse(contents));
+      const encodedContent = await fs.promises.readFile(
+        `${path}/${dirent.name}`
+      );
+      const decodedContent = JSON.parse(encodedContent);
+      const bookList = Object.keys(decodedContent).map((title) => {
+        return {
+          title: title,
+          author: decodedContent[title][0][0],
+          publisher: decodedContent[title][0][1],
+          isbn: decodedContent[title][0][2],
+        };
+      });
+      await db.sync({ force: true });
+      Book.bulkCreate(bookList, { returning: true });
+      console.log("here");
     }
-    // console.log(dirent.name);
   }
 }
+
 read("/Users/elvyyang/Documents/Scrapes/oxford_scrape/book_data").catch(
   console.error
 );
