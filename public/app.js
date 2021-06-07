9daedcdd25b3bf6c0773df9e995e66091a43ee40
Elvy Yang <hyang6@wellesley.edu>
2021-06-07 15:58:08 +0800

    added color

--- public/app.js
+++ public/app.js
@@ -6,7 +6,11 @@ const svg = d3
   .attr("width", width)
   .attr("height", height);
 
-let projection = d3.geoEquirectangular();
+let projection = d3
+  .geoEquirectangular()
+  .center([75, 10])
+  .scale(500)
+  .translate([width / 2, height / 2]);
 //projection function takes [longitude, latitude] & outputs [x,y]
 //use geoEquirectangular or geoMercator
 
@@ -15,10 +19,15 @@ let geoGenerator = d3.geoPath().projection(projection);
 const geoJson = d3.json("custom.geo.json", (error, data) => {
   if (error) console.error(error);
   const dataFeatures = data.features;
+  let index = -1;
   svg
     .selectAll("path")
     .data(dataFeatures)
     .enter()
     .append("path")
+    .attr("class", () => {
+      index++;
+      return `mapColor${dataFeatures[index].properties.mapcolor9}`;
+    })
     .attr("d", geoGenerator);
 });
